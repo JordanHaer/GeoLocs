e6c01fc32ee61c50f1392cd644a4551d343f8129
JordanHaer <jordanhaer@gmail.com>
2024-09-01 12:47:46 +0100

    Modified package.swift file

--- Package.swift
+++ Package.swift
@@ -1,23 +1,23 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "GeoLocs",
+    platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "GeoLocs",
             targets: ["GeoLocs"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "GeoLocs"),
+            name: "GeoLocs",
+            path: "./Sources/GeoLocs",
+            swiftSettings: [.enableExperimentalFeature("StrictConcurrency")]),
         .testTarget(
             name: "GeoLocsTests",
-            dependencies: ["GeoLocs"]),
+            dependencies: ["GeoLocs"],
+            path: "./Tests/GeoLocsTests"),
     ]
 )
